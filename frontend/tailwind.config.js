d25c61e91866f99d16b83fefc527bb66646b2dd6
unknown <yimingsu2007@gmail.com>
2026-02-10 21:50:04 -0800

    Add light theme, custom colors, and Google Fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,30 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-group-yellow',
+    'bg-group-green',
+    'bg-group-blue',
+    'bg-group-purple',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Libre Franklin"', 'sans-serif'],
+      },
+      colors: {
+        tile: '#efefe6',
+        'tile-hover': '#dadad2',
+        'tile-selected': '#5a594e',
+        'group-yellow': '#f9df6d',
+        'group-green': '#a0c35a',
+        'group-blue': '#b0c4ef',
+        'group-purple': '#ba81c5',
+      },
+      maxWidth: {
+        game: '624px',
+      },
+    },
   },
   plugins: [],
 }
